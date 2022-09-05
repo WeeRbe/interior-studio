d638500a2a6851682b4c0c204dd7a77bd8da13f2
WeeRbe <danil.aliev97@gmail.com>
2022-09-05 20:16:33 +0300

    day 3

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,35 @@ const swiper = new Swiper('.popular-slider', {
       type: 'progressbar'
     }
   });
+
+  //DOM load event
+window.addEventListener("DOMContentLoaded", () => {
+
+  const spotlight = document.querySelector('.spotlight');
+
+  let spotlightSize = 'transparent 160px, rgba(9, 9, 9, 0.85) 200px)';
+
+  window.addEventListener('mousemove', e => updateSpotlight(e));
+
+  window.addEventListener('mousedown', e => {
+
+      spotlightSize = 'transparent 130px, rgba(9, 9, 9, 1) 150px)';
+
+      updateSpotlight(e);
+
+  });
+
+  window.addEventListener('mouseup', e => {
+
+      spotlightSize = 'transparent 160px, rgba(9, 9, 9, 1) 200px)';
+
+      updateSpotlight(e);
+
+  });
+
+  function updateSpotlight(e) {
+       
+      spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 40}%, ${spotlightSize}`;
+
+  }
+});
